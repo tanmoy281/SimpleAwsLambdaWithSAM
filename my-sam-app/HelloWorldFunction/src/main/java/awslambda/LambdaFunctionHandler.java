cacf922285580a4e358a684fcf9fb760d54a8aa8
tanmoy <Tanmoy.Banerjee@team.telstra.com>
2022-01-17 19:01:28 +0530

    initial commit

---
+++ my-sam-app/HelloWorldFunction/src/main/java/awslambda/LambdaFunctionHandler.java
@@ -0,0 +1,17 @@
+package awslambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<RequestClass, ResponseClass> {
+
+	public ResponseClass handleRequest(RequestClass input, Context context) {
+		context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        ResponseClass myVal = new ResponseClass();
+        myVal.setGreetings("Hello from POJO - " + input.getName() );
+        return myVal;
+	}
+
+}
---
+++ my-sam-app/HelloWorldFunction/src/main/java/awslambda/RequestClass.java
@@ -0,0 +1,20 @@
+package awslambda;
+
+public class RequestClass {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String firstName) {
+		this.name = firstName;
+	}
+
+	public RequestClass(String name) {
+		this.name = name;
+	}
+
+	public RequestClass() {
+	}
+}
